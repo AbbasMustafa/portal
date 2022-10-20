6aca8a1ee23eb7137eaeae928a1d33116f590f4c
Mustufa Abbas <mustufa.abbas110@gmail.com>
2022-10-20 12:45:17 +0500

    portal base structure

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_mysqldb import MySQL
-# import os
-# from dotenv import load_dotenv, find_dotenv
-# load_dotenv(find_dotenv())
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 
 mysql = MySQL(app)
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_PORT'] = ''
-app.config['MYSQL_DB'] = ''
-app.config['MYSQL_CURSORCLASS'] = ''
-app.config['SECRET_KEY'] = 'aaaaaaaaaaaaaaaaaaaaaaaa'
+app.config['MYSQL_USER'] = os.getenv('DB_USERNAME')
+app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD')
+app.config['MYSQL_HOST'] = os.getenv('DB_HOST')
+app.config['MYSQL_PORT'] = os.getenv('DB_PORT')
+app.config['MYSQL_DB'] = os.getenv('DB_NAME')
+app.config['MYSQL_CURSORCLASS'] = os.getenv('DB_CURSORCLASS')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
