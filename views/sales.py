bf816ce88e809a5f48db7e5ebeb15c6e86e0e0cc
Mustufa Abbas <mustufa.abbas110@gmail.com>
2022-11-02 15:02:01 +0500

    update portal

--- views/sales.py
+++ views/sales.py
@@ -3,7 +3,7 @@ from queries.deleteQuery import SaleQueryDelete
 from queries.updateQuery import SaleQueryUpdate
 from queries.getQuery import SaleQueryGet
 from queries.postQuery import SaleQueryPost
-from utils.auth import login_required, authorize
+from utils.auth import *
 from utils.error_handler import *
 
 
@@ -16,6 +16,17 @@ sale = Blueprint("sale", __name__, static_folder='static', template_folder='temp
 def home_view():
 
     return render_template('sale/index.html')
+
+
+@sale.route('/employee-info/<id>', methods=['GET', 'POST'])
+@login_required_with_param
+@authorize_with_param(my_roles=['Sales'])
+@try_except_with_param
+def employee_info_view(id):
+
+    emp_infor = SaleQueryGet.empInfo(id)
+
+    return render_template('sale/emp-info.html', emp_infor=emp_infor)
 
 
 @sale.route('/create-order')
